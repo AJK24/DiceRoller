92680158ae8f8bdb51ff9a658680e756cfdefdd3
Andrew <akv9509@saintjoe.edu>
2015-02-09 12:12:19 -0500

    This makes this code commented

--- chooseOne.js
+++ chooseOne.js
@@ -1,3 +1,10 @@
+//Andrew Klump
+//February 2015
+//THis is a console version of the dice roller adapted from Capouch's student picker
+
+
+
+
 //var courses = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
 
 var courses = { "Die1": ['1','2','3','4','5','6'], "Die2": ['1','2','3','4','5','6'], "Die3": ['1', '2', '3', '4', '5', '6'], "Die4": ['1','2','3','4','5','6']}
